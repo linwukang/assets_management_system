f5fbbeceea985f02f59da0d8e21bea8748049446
linwukang <linwukang@yahoo.com>
2023-05-23 21:00:42 +0800

    f

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,5 +1,5 @@
 git add .
 git commit -m "f"
 git branch -M main
-git remote add origin git@github.com:linwukang/assets_management_system.git
+git remote set-url origin git@github.com:linwukang/assets_management_system.git
 git push -u origin main
